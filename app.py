c685405760aa5df14d0e50301ee1204cd9df8432
Hongye Li <laurali233@g.ucla.edu>
2024-08-30 19:01:51 -0400

    sync
    sync

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import time
+
+st.set_page_config(
+    page_title="Python Exam 2024",
+    page_icon="🐒",
+    layout="wide",
+)
+
+st.title("CBS 2024 Python Exam Streamlit Dashboard")
+
+placeholder = st.empty()
+
+with placeholder.container():
+    st.markdown("### Student Name: Hongye Li")
+    time.sleep(1)
+
